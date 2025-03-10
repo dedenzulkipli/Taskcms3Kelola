fe2f48207dbc1e6d0830f3b3997d45f267ca5898
januardineky <januardinekyputra@gmail.com>
2025-03-10 14:25:16 +0700

    Menambahkan migration employee

--- app/Models/Employees.php
+++ app/Models/Employees.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Employees extends Model
 {
     //
+    protected $guarded = [];
 }
--- database/migrations/2025_03_10_065930_create_employees_table.php
+++ database/migrations/2025_03_10_065930_create_employees_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('position');
+            $table->string('skills');
+            $table->string('experience');
+            $table->string('photo')->nullable();
             $table->timestamps();
         });
     }
