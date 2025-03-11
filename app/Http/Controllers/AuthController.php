97846e06886ee7e2676d7258055d6175029ed99e
januardineky <januardinekyputra@gmail.com>
2025-03-11 16:52:51 +0700

    Tambahkan AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function auth(Request $request)
+    {
+        $validate = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($validate)) {
+            return redirect ('/dashboard');
+        }
+    }
+}
