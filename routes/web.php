decf8e2ae343e19d675b82a559fcf4c2e4f84a45
dedenzulkipli <dedenzulkipli6@gmail.com>
2025-03-11 17:02:10 +0700

    Ganti landing page

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('landing.index');
 });
 Route::get('/portfolio-details', function () {
-    return view('portfolio-details');
+    return view('landing.portfolio-details');
 });
 Route::get('/service-details', function () {
-    return view('service-details');
+    return view('landing.service-details');
+});
+Route::get('/blog-details', function () {
+    return view('landing.blog-details');
+});
+Route::get('/blog', function () {
+    return view('landing.blog');
 });
