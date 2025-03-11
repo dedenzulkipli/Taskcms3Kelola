013a741ff932439ed84ebe73240f4614082b2e4e
geusanraditya17 <radityageusan12@gmail.com>
2025-03-11 17:02:00 +0700

    revisi

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 Route::get('/portfolio-details', function () {
     return view('portfolio-details');
 });
@@ -19,4 +19,13 @@ Route::get('/indexAdmin', function () {
 });
 Route::get('/datatables', function () {
     return view('datatables');
+});
+
+
+Route::get('coba', function() {
+    return view('admin.coba');
+});
+
+Route::get('/', function() {
+    return view('landing.index');
 });
